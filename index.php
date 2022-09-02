9e1d1cceec82e35695f21e9fb8106b729a99b7c6
Tianci <2016838087@qq.com>
2022-09-02 14:46:07 +0800

    首次提交 By 宾天赐 2022年9月2日14点46分

---
+++ index.php
@@ -0,0 +1,54 @@
+﻿<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <title>TianciWallpaper</title>
+
+    <meta name="keywords" content="天赐在线壁纸,电脑桌面壁纸,高清电脑桌面壁纸,电脑桌面壁纸下载,电脑高清桌面壁纸,天赐电脑壁纸" />
+    <meta name="description" content="天赐在线壁纸提供高清电脑壁纸下载,包括美女,游戏,动漫,动物,汽车,体育,广告,影视,明星,风景,绘画,节日,花卉等经典壁纸"/>
+
+    <!-- jQuery文件 -->
+    <!-- <script src="https://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>链接作废 -->
+    <!-- <script src="https://lib.baomitu.com/jquery/1.11.3/jquery.min.js"></script> -->
+    <script type="text/javascript" src="js/1.11.3.jquery.min.js"></script>
+
+    <!--滚动加载插件-->
+    <script type="text/javascript" src="js/jquery.lazyload.min.js"></script>
+
+    <!--全屏滚动插件-->
+    <script type="text/javascript" src="js/jquery.onepage-scroll.min.js"></script>
+
+    <!--页面核心js文件-->
+    <script type="text/javascript" src="js/wallpaper.js"></script>
+    
+    <link rel="stylesheet" href="css/wallpaper.css">
+</head>
+<body>
+
+<div class="banner">
+    <h1 class="webTitle" title="Copyright &copy; TianciQQ：2016838087">天赐电脑壁纸</h1>
+    <ul id="banner">
+        <li onclick="loadData('360new', true);changeTitle(this)">最新壁纸</li>
+        <li class="tags">
+            分类壁纸
+            <ul id="tags"></ul>
+        </li>
+        <li onclick="loadData('bing', true);changeTitle(this)">必应美图</li>
+        <li onclick="loadData('ciba', true);changeTitle(this)" title="金山词霸每日一句壁纸">每日英语</li>
+        <li><a href="https://lovetianci.cn" target="_blank">天赐博客</a></li>
+    </ul>
+</div>
+
+<div class="container">
+    
+    <div class="jigsaw" id="walBox"></div>  <!-- id="walBox" -->
+    
+    <a id="toolBall" target="_blank" href="javascript:void(0);" class="uptoTop"></a>
+    
+    <!-- <div id="loadmore">湘ICP备19020367号-1<a href="http://www.beian.miit.gov.cn/state/outPortal/loginPortal.action;jsessionid=d26CZT9UyzDjMiz-1NEx_Dx5-vRUF4z-XE3YFnhqcZvoCDnQavAe!1870117190">工信部</a></div> -->
+
+</div><!-- class="container" -->
+
+</body>
